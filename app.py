aa97f42727446b1e9ff1436380b3e1057290a5b7
Harsh-Chugh <harshchugh11702@gmail.com>
2023-10-02 12:46:54 +0530

    add get_similar

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import os
 from urllib.parse import urlparse
 
 
-client = Metaphor(api_key="METAPHOR_API_KEY")
+# client = Metaphor(api_key="METAPHOR_API_KEY")
+client = Metaphor(api_key="6f209af2-f0c9-46c3-b5e8-f21791576865")
 
-token = 'BARD_API_KEY'
+# token = 'BARD_API_KEY'
+token = 'bgipRD2u17OfJ4l-in6HtjKP5poyLrai9B7bu8F2mAvGepA445aAz55WiR4IecaOzucvqw.'
 # bard = bardapi.core.Bard(token)
 
 TOPIC_MESSAGE = "You are an intelligent assistant. I will provide you a text and you need to find out the content category of that text in no more than 4 words and output only that no other words: "
@@ -22,7 +24,7 @@ def search():
 
     # topic = "cryptocurrency"
     print("topic: ", topic)
-    relevant_response = client.search("Here are 10 most reliable websites about " + topic, 
+    relevant_response = client.search("This is the best website to learn about: " + topic, 
         num_results= 10
     )
 
@@ -47,9 +49,52 @@ def search():
         include_domains=domain_array
     )
 
-    # print fetched blog title and url
-    # for result in response.results:
-    #     print(result.title, result.url)
+    id=response.results[0].id
+    print(id, end="\n\n\n")
+
+    response = client.get_contents(id)
+
+    blog_content = response.contents[0].extract
+    print(blog_content, end="\n\n\n")
+
+    print("######\n\n\n")
+
+    data = re.sub(r'<.*?>', '', blog_content)
+    print(data, end="\n\n\n")
+    print(response.contents[0].url)
+
+    return response.contents[0]
+
+
+
+
+
+def get_similar_link(previous):
+    # print("##\n\n", previous, "##\n\n")
+    blog_content = previous.extract
+    data = re.sub(r'<.*?>', '', blog_content)
+
+    topic = bardapi.core.Bard(token).get_answer(TOPIC_MESSAGE + data)['content']
+    print("topic: ", topic)
+    
+    
+    relevant_response = client.search("This is the best website to learn about: " + topic, 
+        num_results= 10
+    )
+    url_array=[]
+    for result in relevant_response.results:
+        url_array.append(result.url)
+    print(url_array, end="\n\n\n")
+
+    domain_array=[]
+    for url in url_array:
+        domain_array.append(urlparse(url).netloc)
+    print(domain_array, end="\n\n\n")
+
+    response = client.find_similar(previous.url, 
+        num_results=1,
+        include_domains=domain_array
+    )
 
     id=response.results[0].id
     print(id, end="\n\n\n")
@@ -65,25 +110,24 @@ def search():
     print(data, end="\n\n\n")
     print(response.contents[0].url)
 
-    return response.contents[0].url
+    return response.contents[0]
 
-def get_similar_link(URL):
-    print(1)
+
 def get_random_result():
     print(2)
 
 
 user_input = "random"
-URL = ""
+previous = ""
 
 while user_input != 'quit':
     input()
     if(user_input == "similar"):
-        URL = get_similar_link(URL)
+        previous = get_similar_link(previous)
     elif(user_input == "random"):
-        URL = get_random_result()
+        previous = get_random_result()
     else:
-        URL = search()
+        previous = search()
 
     user_input = input("random, similar or quit: ")
         
